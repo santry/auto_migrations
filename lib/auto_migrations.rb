santry/auto_migrations

commit 6bdbdabe0c9a40b6c207a026ffb4ad5476e67dbc

Now works with new Rails schema version system

lib/auto_migrations.rb
@@ -134,7 +134,7 @@ module AutoMigrations
     end
     
     def update_schema_version(version)
-      ActiveRecord::Base.connection.update("UPDATE #{ActiveRecord::Migrator.schema_info_table_name} SET version = #{version}")
+      ActiveRecord::Base.connection.update("INSERT INTO schema_migrations VALUES ('#{version}')")
     end
   
   end

commit 650473e7f3fdb4598524579ab41898162b8a4e1b

Automatically generate migration version number for auto migrations
Commit 6bdbdabe caused auto_migrations to require a new schema
version number for each auto migration. This patch uses the
datetime-based numbering scheme from Rails 2.1 migrations to
automatically generate a new schema version number for the
auto migration and updates the Schema.define statement in
db/schema.rb with this version number.

lib/auto_migrations.rb
@@ -5,7 +5,7 @@ module AutoMigrations
     class << ActiveRecord::Schema
       alias :old_define :define
       attr_accessor :version
-      def define(info={}, &block) @version=info[:version]; instance_eval(&block) end
+      def define(info={}, &block) @version = Time.now.utc.strftime("%Y%m%d%H%M%S"); instance_eval(&block) end
     end
   
     load(File.join(RAILS_ROOT, 'db', 'schema.rb'))
@@ -135,6 +135,11 @@ module AutoMigrations
     
     def update_schema_version(version)
       ActiveRecord::Base.connection.update("INSERT INTO schema_migrations VALUES ('#{version}')")
+
+      schema_file = File.join(RAILS_ROOT, "db", "schema.rb")
+      schema = File.read(schema_file)
+      schema.sub!(/:version => \d+/, ":version => #{version}")
+      File.open(schema_file, "w") { |f| f << schema }
     end
   
   end